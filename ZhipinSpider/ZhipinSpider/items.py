85bd139cf085f6b46b4590ad63049146f4173b48
Jack <188075841@qq.com>
2019-07-01 20:51:05 +0800

    合并冲突

--- ZhipinSpider/ZhipinSpider/items.py
+++ ZhipinSpider/ZhipinSpider/items.py
@@ -11,13 +11,10 @@ import scrapy
 class ZhipinspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-<<<<<<< HEAD
-=======
     title = scrapy.Field()
     salary = scrapy.Field()
     compony = scrapy.Field()
     work_addr = scrapy.Field()
     seniority = scrapy.Field()
     education = scrapy.Field()
->>>>>>> 4d818188039a4864460a0150c2024c8d283ebbe3
     pass
